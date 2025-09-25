81266d3441886a1fb762667d421f2e2718b66b48
Zacatenango <zacatenango@github.com>
2025-09-24 18:48:11 -0600

    Iniciando clase 6

---
+++ clase6/tarea6.py
@@ -0,0 +1,5 @@
+# El problema del vendedor viajero. Sea G un grafo de aristas ponderados cuyos nodos representen
+# lugares geográficos. Sacar la ruta más corta posible que visite cada ciudad exactamente una sola
+# vez y regrese a la ciudad de origen.
+
+# Resulta
