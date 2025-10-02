b25514b4bbc0f0d54f6c117f2835f4d8bf29bc5a
Zacatenango <zacatenango@github.com>
2025-10-01 18:59:11 -0600

    Código listo

--- tarea6/tarea6.py
+++ tarea6/tarea6.py
@@ -14,8 +14,8 @@ distancias = pandas.read_excel("probarSimAnn.xlsx", sheet_name="8c", index_col=0
 # Ajustes del programa, organizados en un diccionario de ajustes
 SETTS = \
 {
-   "T_inicial": 1000,
-   "T_final": 1,
+   "T_inicial": 10000,
+   "T_final": 0.1,
    "n": 100
 }
 
@@ -24,22 +24,19 @@ def Ln(x):
    return numpy.log(x)
 def func_templado(T0, t):
    #return T0 / (1+t)
-   return T0 / Ln(1+t)
+   #return T0 / Ln(1+t)
+   return T0 * (0.95**t)
 
 
 
 # Nuestra configuración estará encapsulada en una clase
 class Configuracion():
-   # Secuencia inicial al azar
    secuencia = [ "Tijuana", "Mérida", "GDL", "México", "León", "Monterrey", "Tapachula", "Chihuahua" ]
-   # Secuencia óptima para probar, su distancia es 9524
-   secuencia_optima = [ "Tijuana", "GDL", "León", "México", "Mérida", "Tapachula", "Monterrey", "Chihuahua" ]
+   secuencia_optima_9524 = [ "Tijuana", "GDL", "León", "México", "Mérida", "Tapachula", "Monterrey", "Chihuahua" ]
 
-   # Comienzo barajeando mi secuencia inicial
    def barajear_secuencia(self):
       self.secuencia = numpy.random.choice(self.secuencia, len(self.secuencia), replace=False).tolist()
 
-   # Fórmula para sacar la "energía"
    def energia(self):
       distancia = 0
       for X in range(len(self.secuencia)):
@@ -48,7 +45,6 @@ class Configuracion():
          distancia += distancias[ciudad_actual][ciudad_siguiente]
       return distancia
    
-   # Función para sacar una configuración cercana
    def sacar_configuracion_cercana(self):
       config_nueva = Configuracion()
       # Hay que asegurarnos de que la secuencia nueva sea una lista nueva y 
@@ -66,7 +62,7 @@ class Configuracion():
 T = SETTS["T_inicial"]
 t = 0
 n = SETTS["n"]
-energias = []
+historial_enfriamiento = []
 iters = 0
 C = Configuracion()
 C.barajear_secuencia()
@@ -83,11 +79,12 @@ while T > SETTS["T_final"]:
    t += 1
    T = func_templado(SETTS["T_inicial"], t)
    iters += 1
-   energias.append(C.energia())
+   historial_enfriamiento.append(C.energia())
 
-# Terminado el proceso, obtengo mi secuencia y saco su energía
+# Terminado el proceso, obtengo mi secuencia, saco su energía, y grafico mi historial
+# de enfriamiento
 print(f"La secuencia final es {json.dumps(C.secuencia)}")
 print(f"Su distancia es {C.energia()}")
 print(f"Iteraciones totales: {iters}")
-pyplot.plot(energias)
+pyplot.plot(historial_enfriamiento)
 pyplot.show()
