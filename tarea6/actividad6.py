99526c255b762876443a48b3338ecb9a662565c5
Zacatenango <zacatenango@github.com>
2025-10-01 18:59:42 -0600

    Iniciando actividad 6

---
+++ tarea6/actividad6.py
@@ -0,0 +1,90 @@
+import pandas
+import numpy
+import math
+import json
+import matplotlib.pyplot as pyplot
+
+# Planteamiento del problema:
+# Resolver con templado simulado el Problema del Vendedor Viajero con 8 
+# ubicaciones, y la matriz de distancia en kilómetros ubicada en 
+# probasSimAnn.xlsx, en la hoja "8c"
+distancias = pandas.read_excel("probarSimAnn.xlsx", sheet_name="8c", index_col=0, nrows=9)
+#print(f"{distancias}\n")
+
+# Ajustes del programa, organizados en un diccionario de ajustes
+SETTS = \
+{
+   "T_inicial": 10000,
+   "T_final": 0.1,
+   "n": 100
+}
+
+# Definimos una función de templado
+def Ln(x):
+   return numpy.log(x)
+def func_templado(T0, t):
+   #return T0 / (1+t)
+   #return T0 / Ln(1+t)
+   return T0 * (0.95**t)
+
+
+
+# Nuestra configuración estará encapsulada en una clase
+class Configuracion():
+   secuencia = [ "Tijuana", "Mérida", "GDL", "México", "León", "Monterrey", "Tapachula", "Chihuahua" ]
+   secuencia_optima_9524 = [ "Tijuana", "GDL", "León", "México", "Mérida", "Tapachula", "Monterrey", "Chihuahua" ]
+
+   def barajear_secuencia(self):
+      self.secuencia = numpy.random.choice(self.secuencia, len(self.secuencia), replace=False).tolist()
+
+   def energia(self):
+      distancia = 0
+      for X in range(len(self.secuencia)):
+         ciudad_actual = self.secuencia[X]
+         ciudad_siguiente = self.secuencia[ (X+1)%len(self.secuencia) ]
+         distancia += distancias[ciudad_actual][ciudad_siguiente]
+      return distancia
+   
+   def sacar_configuracion_cercana(self):
+      config_nueva = Configuracion()
+      # Hay que asegurarnos de que la secuencia nueva sea una lista nueva y 
+      # virgen, no una referencia a la anterior
+      config_nueva.secuencia = self.secuencia.copy()
+      indice_1 = numpy.random.randint(0, len(config_nueva.secuencia))
+      indice_2 = numpy.random.randint(0, len(config_nueva.secuencia))
+      # Intercambiamos 2 elementos al azar
+      config_nueva.secuencia[indice_1], config_nueva.secuencia[indice_2] = \
+         config_nueva.secuencia[indice_2], config_nueva.secuencia[indice_1]
+      return config_nueva
+
+
+# Ahora estoy listo para correr el templado simulado
+T = SETTS["T_inicial"]
+t = 0
+n = SETTS["n"]
+historial_enfriamiento = []
+iters = 0
+C = Configuracion()
+C.barajear_secuencia()
+
+
+while T > SETTS["T_final"]:
+   for X in range(n):
+      Cprima = C.sacar_configuracion_cercana()
+      delta_E = Cprima.energia() - C.energia()
+      q = math.exp(-delta_E / T)
+      p = numpy.random.random()
+      if delta_E < 0 or p < q:
+         C = Cprima
+   t += 1
+   T = func_templado(SETTS["T_inicial"], t)
+   iters += 1
+   historial_enfriamiento.append(C.energia())
+
+# Terminado el proceso, obtengo mi secuencia, saco su energía, y grafico mi historial
+# de enfriamiento
+print(f"La secuencia final es {json.dumps(C.secuencia)}")
+print(f"Su distancia es {C.energia()}")
+print(f"Iteraciones totales: {iters}")
+pyplot.plot(historial_enfriamiento)
+pyplot.show()
