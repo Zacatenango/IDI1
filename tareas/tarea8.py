5018816264c8c40aa0dad5df4392a296c5e732e1
Zacatenango <zacatenango@github.com>
2025-10-15 17:43:55 -0600

    Continuando

--- tareas/tarea8.py
+++ tareas/tarea8.py
@@ -1,5 +1,6 @@
 import json
 
+
 class Pila:
    def __init__(self, capacidad=100):
       self.data = []
@@ -9,7 +10,7 @@ class Pila:
       if len(self.data) < self.capacity:
          self.data.append(dato)
    
-   def pop(self, dato):
+   def pop(self):
       return self.data.pop()
 
    def peek(self):
@@ -27,9 +28,43 @@ class Pila:
    def print(self):
       return json.dumps(self.data)
 
+
 pila1 = Pila(50)
 for X in range(1, 50, 2):
    pila1.push(X)
-print(f"{pila1.print()}")
+print(f"Stack: {pila1.print()}")
 
+
+class Cola:
+   def __init__(self, capacidad=100):
+      self.data = []
+      self.capacity = capacidad
+   
+   def enqueue(self, dato):
+      self.data.insert(0, dato)
+   
+   def dequeue(self):
+      return self.data.pop()
+
+   def peek(self):
+      return self.data[0]
+   
+   def empty(self):
+      return len(self.data) == 0
+   
+   def full(self):
+      return len(self.data) == self.capacity
+   
+   def count(self):
+      return len(self.data)
+   
+   def print(self):
+      return json.dumps(self.data)
+   
+
+cola1 = Cola(50)
+while not pila1.empty():
+   cola1.enqueue(pila1.pop())
+print(f"Cola: {cola1.print()}")
+print(f"Stack vacío: {pila1.print()}")
 
