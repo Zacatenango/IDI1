6035dae9fec93b7803655899e3effc24ddf3ac15
Zacatenango <zacatenango@github.com>
2025-10-15 18:34:55 -0600

    Iniciando actividad 7

---
+++ actividades/actividad7.py
@@ -0,0 +1,16 @@
+# Los árboles son estructuras no indexadas; es decir, no hay un modo riguroso de asignarle un orden
+# o índice.
+
+# Los árboles son no lineales: si recorro aristas en línea recta, no recorreré toda la estructura.
+
+# Rama: cualquier secuencia lineal dentro del árbol
+# Rama principal: cualquier rama que vaya desde la raíz hasta una hoja
+# Profundidad: la longitud de la rama principal más larga, siempre es de base 1
+# Nivel: vértices que estén a la misma distancia de grafo de la raíz, puede ser de base 0
+# Padre e hijo: 2 nodos conectados por un arista (en referencia al árbol genealógico)
+
+# Árbol binario: árbol donde cada padre tiene máximo 2 hijos
+# Arbol n-ario: árbol donde cada padre tiene máximo n hijos
+# Árbol completo: árbol donde cada padre tiene todos los hijos posibles
+# Árbol balanceado: árbol donde todas las hojas están en el mismo nivel
+# 
