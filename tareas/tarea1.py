76656bf45916e32072ea4c9853619d599493d01b
Zacatenango <zacatenango@github.io>
2025-11-19 16:34:57 -0600

    Repasando tareas

--- tareas/tarea1.py
+++ tareas/tarea1.py
@@ -1,11 +1,23 @@
 import pandas
 
+# A partir de un documento de Excel, cargamos un dataframe ubicado en la hoja "vt", donde la columna
+# de índice es la primera
 vt = pandas.read_excel("datosCT.xlsx", sheet_name="vt", index_col=0)
 
+# Suponga que hay un olvido y no se tomó en cuenta un ingreso mensual de $1000 en todos los meses
+# Sume 1000 al ingreso en cada mes
 vt["Ingreso"] += 1000
+
+# Saque la utilidad en una columna nueva
 vt["Utilidad"] = vt["Ingreso"] - vt["Gasto"]
+
+# Saque el promedio de gasto en los meses en los que la utilidad fue negativa
 promedio_gasto_utilidad_negativa = vt[ vt["Utilidad"] < 0 ]["Gasto"].mean()
+
+# Saque el ingreso total del año
 ingreso_total = vt["Ingreso"].sum()
+
+# Saque cuáles meses fueron los de utilidad positiva
 meses_utilidad_positiva = vt[ vt["Utilidad"] > 0 ].index
 
 print(f"-------------- Sección vt --------------")
--- tareas/tarea2.py
+++ tareas/tarea2.py
@@ -1,19 +1,56 @@
 import numpy
 
+# Lista L con los primeros 50 números naturales nones
 L = [X for X in range(50) if (X % 2) == 0]
+
+# Arreglo A a partir de la lista L
 A = numpy.array(L)
+
+# Imprimir el séptimo item de A, las posiciones 3-9, y los últimos 3 en orden descendente
 print(f"Séptimo item de A: {A[7]}; Posiciones 3-9: {A[3:10]}; Últimos 3 en orden descendente: {A[-1:-3]}")
+
+# Arreglo B del 20 al 5, de 3 en 3
 B = numpy.array(range(20, 5, 3))
+
+# C = arreglo B volteado
 C = B[::-1]
+
+# Arreglo D de 100 números del 2 al 30
 D = numpy.linspace(2, 30, 100)
+
+# Tirar el tamaño del paso
 print(f"Tamaño de paso para generar D: {D[1] - D[0]}")
-E = numpy.linspace(5, 100, dtype="int")
+
+# E = 100 números del 5 al 100, de tipo entero
+E = numpy.linspace(5, 100, 100, dtype="int")
+
+# I = elmeentos de A que sean múltiplos de 3
 I = A[ (A % 3 == 0) ]
+
+# F = elementos de D tomados por filas (estilo C) en matriz de 10 x 10
 F = numpy.reshape(D, (10,10), "C")
+
+# G = idem, pero por columnas (estilo Fortran)
 G = numpy.reshape(D, (10,10), "F")
+
+# M = elementos de G de las filas 1 y 2, columnas 3-6
 M = G[0:2, 2:6]
+
+# N = elementos de G al cuadrado, menos 3
 N = (G**2) - 3
+
+# Tirar "SI" si algún elemento de N es divisible entre 17, "NO" de lo contrario
 print("SI" if numpy.any(N % 17 == 0) else "NO")
+
+# Tirar tercera fila de F, y cuarta columna de G
 print(f"{F[2]}, {G[:,3]}")
+
+# H = F + G
 H = F + G
-H[H<30] = 0; K = H
+
+# Cambiar los elementos de H menores de 30 por ceros y guardar la matriz resultante en K
+H[H<30] = 0
+K = H
+
+# Tirar la cantidad de columnas de K cuya suma sea mayor de 500
+
